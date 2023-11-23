552419cb4bcb77d3af56b98b3539b7a93b6b75a7
a.polischuk <a.polischuk@adriver.ru>
2023-11-23 17:33:15 +0300

    refactor: delete comments, refactor code style

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,12 +1,11 @@
-import React, { useMemo, useState, useEffect, FC } from "react";
+import { useState } from "react";
 import { useQuery } from '@apollo/client';
+
 import { ALL_SHIPS } from '../../apollo/korabli';
 import { ShipLittleCard } from '../../components/ShipLittleCard/ShipLittleCard';
 import { IShips } from '../../types/shipType';
-
-import classes from './MainPage.module.css';
 import { Filters } from '../../components/Filters/Filters';
-
+import classes from './MainPage.module.css';
 
 export const MainPage = () => {
   const { loading, error, data } = useQuery(ALL_SHIPS);
@@ -15,57 +14,57 @@ export const MainPage = () => {
   const [levelFilter, setLevelFilter] = useState('ascending');
 
   if (loading) {
-    return <h2>loading</h2>
+    return <h2>loading...</h2>
   }
 
   if (error) {
-    return <h2>error</h2>
+    return <h2>error...</h2>
   }
 
   return (
     <>
-    <Filters 
-      nation={nation}
-      classType={classType}
-      levelFilter={levelFilter}
-      setNation={setNation}
-      setClassType={setClassType}
-      setLevelFilter={setLevelFilter}
-      
-     />
-    <div className={classes.shipsContainer}>
-      {data.vehicles && (
-          data.vehicles
-          .filter((ship: IShips, index: number, shipsArray: IShips[]) => {
-            if (nation === 'all-nations') return shipsArray;
-            return ship.nation.title === nation;
-          })
-          .filter((ship: IShips, index: number, shipsArray: IShips[]) => {
-            if (classType === 'all-classes') return shipsArray;
-            return ship.type.title === classType;
-          })
-          .sort((firstShip: IShips, secondShip: IShips) => {
-            if (levelFilter === 'ascending') {
-              return firstShip.level - secondShip.level;
-            }
-            if (levelFilter === 'descending') {
-              return secondShip.level - firstShip.level;
-            }
-          })
-          .map((ship: IShips)=> (
-            <ShipLittleCard 
-              key={ship.id} 
-              id={ship.id} 
-              title={ship.title} 
-              type={ship.type.title}
-              nation={ship.nation.title}
-              level={ship.level}
-              description={ship.description}
-              image={ship.icons.medium}
-              />
-          ))
-        )}
-    </div>
+      <Filters 
+        nation={nation}
+        classType={classType}
+        levelFilter={levelFilter}
+        setNation={setNation}
+        setClassType={setClassType}
+        setLevelFilter={setLevelFilter}
+      />
+      <div className={classes.shipsContainer}>
+        {data.vehicles && (
+            data.vehicles
+            .filter((ship: IShips, index: number, shipsArray: IShips[]) => {
+              if (nation === 'all-nations') return shipsArray;
+              return ship.nation.title === nation;
+            })
+            .filter((ship: IShips, index: number, shipsArray: IShips[]) => {
+              if (classType === 'all-classes') return shipsArray;
+              return ship.type.title === classType;
+            })
+            .sort((firstShip: IShips, secondShip: IShips) => {
+              if (levelFilter === 'ascending') {
+                return firstShip.level - secondShip.level;
+              }
+              if (levelFilter === 'descending') {
+                return secondShip.level - firstShip.level;
+              }
+            })
+            .map((ship: IShips)=> (
+              <ShipLittleCard 
+                key={ship.id} 
+                id={ship.id} 
+                title={ship.title} 
+                type={ship.type.title}
+                nation={ship.nation.title}
+                level={ship.level}
+                description={ship.description}
+                image={ship.icons.medium}
+                flagImage={ship.nation.icons.small}
+                />
+            ))
+          )}
+      </div>
     </>
   )
 }
